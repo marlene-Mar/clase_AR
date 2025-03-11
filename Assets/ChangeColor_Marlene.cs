6e0b470b80432ae8a68ca798d413648599a75c9f
Marlene Mariana De La Cruz Padilla <mardlc717@outlook.com>
2025-03-11 10:16:21 -0600

    Practica 3
    Cambio de color y texturas de manera aleatoria, así como asignación de un nuevo diseño de botón.

--- Assets/ChangeColor_Marlene.cs
+++ Assets/ChangeColor_Marlene.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class ChangeColor_Marlene : MonoBehaviour
 {
-    public GameObject model;
-    public Color color;
-    public Material colorMaterial;
+    public GameObject model; //Modelo al que se le aplicará el cambio de color
+    public Color [] colors; //Colores que se le aplicarán al modelo
+    public Material[] materials; //Materiales que se le aplicarán al modelo
 
-    //Para hacer que cambie de color cada que se aprieta botón
-    private Color[] colors = { Color.red, Color.green, Color.yellow, Color.black, Color.magenta };
+    private int contText_Cambio = 0; //Contador para cambiar de textura
+
+    /*---------------------------PRACTICA 2------------------------------------
+    //public Material colorMaterial;
+
+    //Para hacer que cambie de color cada que se aprieta botón 
+    //private Color[] colors = { Color.red, Color.green, Color.yellow, Color.black, Color.magenta };
+
+    ---------------------------------------------------------------------------*/
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //model.GetComponent<Renderer>().material.color = Color.red;
+        /*------------------------PRACTICA 2-------------------------------------
+        Cambio de color al modelo en cuanto se inicia la aplicación
+        model.GetComponent<Renderer>().material.color = Color.red;
+        -----------------------------------------------------------------------*/
     }
 
     // Update is called once per frame
@@ -25,8 +36,21 @@ public class ChangeColor_Marlene : MonoBehaviour
 
     public void ChangeColor_Bot()
     {
+        contText_Cambio++; //Incrementa el contador para cambiar de textura
+        //Si el contador es mayor o igual a la cantidad de texturas, se reinicia
+        if (contText_Cambio >= materials.Length)
+        {
+            contText_Cambio = 0;
+        }
+        //Se le asigna un color aleatorio al material
+        materials[contText_Cambio].color = colors[Random.Range(0, colors.Length)];
+        //Se le asigna un material aleatorio al modelo
+        model.GetComponent<Renderer>().material = materials[Random.Range(0, materials.Length)];
+
+        /*--------------------------PRACTICA 2------------------------------------
         color = colors[Random.Range(0, colors.Length)];
         model.GetComponent<Renderer>().material.color = color;
-        colorMaterial.color = color; 
+        colorMaterial.color = color;
+        ------------------------------------------------------------------------*/
     }
 }
